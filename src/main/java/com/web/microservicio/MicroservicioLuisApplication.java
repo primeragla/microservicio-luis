d4f372b35e7f9402f9bea353362b4f2cb5a7d704
Luis <primeragla@gmail.com>
2020-01-15 21:41:47 +0100

    first commit

---
+++ src/main/java/com/web/microservicio/MicroservicioLuisApplication.java
@@ -0,0 +1,13 @@
+package com.web.microservicio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioLuisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicioLuisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/web/microservicio/MicroservicioLuisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.microservicio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioLuisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
